c92eb8df2bd152b216ec896aeff247fdbefe7e4b
Irene Brignoli <ire.brignoli@gmail.com>
2023-02-23 16:17:02 +0100

    inzio js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,74 @@ Nota:
 Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
 
 */
+
+
+const distanzaDom = document.getElementById('distanza');
+const distanzaInserita = distanzaDom.value;
+
+const etaDom = document.getElementById('eta');
+const etaInserita = etaDom.value;
+
+const buttonDom = document.querySelector('#leggi-form');
+
+buttonDom.addEventListener('click', 
+  function () {
+
+    console.log('Distanza inserita ' + distanzaInserita);
+  
+    console.log('Età inserita ' + etaInserita);
+  }
+);
+
+
+/*
+
+const distance = parseInt(prompt('Inserisci la distanza che vuoi percorrere (km)'));
+console.log('Distanza: ' + distanza);
+
+const eta = parseInt(prompt('Inserisci la tua età'));
+console.log('Età: ' + eta);
+
+let prezzoAlKm = 0.21;
+
+let prezzoBigliettoBase = prezzoAlKm * distanza;
+prezzoBigliettoBase = prezzoBigliettoBase.toFixed(2);
+console.log('Prezzo biglietto normale: ' + prezzoBigliettoBase);
+
+let scontoGiovani = (prezzoBigliettoBase / 100) * 20;
+console.log('Sconto giovani: ' + scontoGiovani);
+
+let scontoAnziani = (prezzoBigliettoBase / 100) * 40;
+console.log('Sconto anziani: ' + scontoAnziani);
+
+let bigliettoGiovani = prezzoBigliettoBase - scontoGiovani;
+bigliettoGiovani = bigliettoGiovani.toFixed(2);
+
+let bigliettoAnziani = prezzoBigliettoBase - scontoAnziani;
+bigliettoAnziani = bigliettoAnziani.toFixed(2);
+
+
+if (eta < 18) {
+  document.getElementById('prezzo-viaggio').innerHTML = 'Il prezzo è di € ' + bigliettoGiovani + '<p>Hai uno sconto del 20% perché sei minorenne!</p>';
+  
+} else if (eta > 65) {
+  document.getElementById('prezzo-viaggio').innerHTML = 'Il prezzo è di € ' + bigliettoAnziani + '<p>Hai uno sconto del 40% perché sei over 65!</p>';
+
+} else {
+  document.getElementById('prezzo-viaggio').innerHTML = 'Il prezzo è di € ' + prezzoBigliettoBase;
+ 
+}
+
+*/
+
+
+
+/*
+if (eta < 18) {
+  risultato = prezzoBigliettoBase - scontoGiovani;
+} else if (eta > 65) {
+  risultato = prezzoBigliettoBase - scontoAnziani;
+} else {
+  risultato = prezzoBigliettoBase;
+}
+*/
